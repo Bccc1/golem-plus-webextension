75d3d8614b751c256d2195716f97de148507814a
Bccc1 <9332668+Bccc1@users.noreply.github.com>
2018-01-28 21:11:11 +0100

    added homepage

--- golemde-forum.user.js
+++ golemde-forum.user.js
@@ -3,6 +3,8 @@
 // @author Bccc1
 // @version  1
 // @license MIT
+// @homepageURL https://github.com/Bccc1/golem-plus-webextension
+// @supportURL https://github.com/Bccc1/golem-plus-webextension/issues
 // @grant    none
 // @match    https://forum.golem.de/kommentare/*read.html*
 // @match    https://forum.golem.de/read.php?*
