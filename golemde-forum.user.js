7653a2d19080ab1d4329e739c7096343a8b1d003
Bccc1 <davidruppelt@gmx.de>
2018-02-04 22:12:27 +0100

    Added @match for bolzplatz

--- golemde-forum.user.js
+++ golemde-forum.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name     Golem.de Forum+
 // @author Bccc1
-// @version  1.0.0
+// @version  1.0.1
 // @license MIT
 // @homepageURL https://github.com/Bccc1/golem-plus-webextension
 // @supportURL https://github.com/Bccc1/golem-plus-webextension/issues
 // @grant    none
 // @match    https://forum.golem.de/kommentare/*read.html*
 // @match    https://forum.golem.de/read.php?*
+// @match    https://forum.golem.de/sonstiges/bolzplatz/*read.html*
 // ==/UserScript==
 
 
